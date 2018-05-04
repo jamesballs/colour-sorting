c3c229b0a5c478e4d6fc866612013bfc0043ff8c
James Ball <james@jamesball.me>
2018-05-04 21:32:07 +0100

    Moved project to GitHub.

---
+++ Colour Sorting/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Colour_Sorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnQuick = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(126, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start Bubble Sort";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 128);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Passes per ms:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(264, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 23);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total time taken:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(128, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(128, 371);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 371);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Total time taken:";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(264, 209);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 23);
+            this.label7.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(153, 209);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(105, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Passes per ms:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 240);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(512, 128);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnQuick
+            // 
+            this.btnQuick.Location = new System.Drawing.Point(12, 206);
+            this.btnQuick.Name = "btnQuick";
+            this.btnQuick.Size = new System.Drawing.Size(126, 23);
+            this.btnQuick.TabIndex = 6;
+            this.btnQuick.Text = "Start Quick Sort";
+            this.btnQuick.UseVisualStyleBackColor = true;
+            this.btnQuick.Click += new System.EventHandler(this.btnQuick_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 417);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnQuick);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnQuick;
+    }
+}
+
---
+++ Colour Sorting/Form1.cs
@@ -0,0 +1,465 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colour_Sorting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int width = 1024;
+            int height = 128;
+
+            Bitmap img = new Bitmap(width, height);
+
+            pictureBox1.Width = width;
+            pictureBox1.Height = height;
+
+            Random rnd = new Random();
+
+            int[] rgb;
+
+            for (int row = 0; row < img.Width; row++)
+            {
+                rgb = new int[3];
+
+                int[] zeroonetwo = { 0, 1, 2 };
+
+                int onetosix = rnd.Next(1, 7);
+
+                switch (onetosix)
+                {
+                    case 1:
+                        zeroonetwo = new int[] { 0, 1, 2 };
+                        break;
+                    case 2:
+                        zeroonetwo = new int[] { 1, 0, 2 };
+                        break;
+                    case 3:
+                        zeroonetwo = new int[] { 2, 0, 1 };
+                        break;
+                    case 4:
+                        zeroonetwo = new int[] { 2, 1, 0 };
+                        break;
+                    case 5:
+                        zeroonetwo = new int[] { 0, 2, 1 };
+                        break;
+                    case 6:
+                        zeroonetwo = new int[] { 1, 2, 0 };
+                        break;
+                }
+
+                rgb[zeroonetwo[0]] = rnd.Next(0, 256);
+                rgb[zeroonetwo[1]] = 0;
+                rgb[zeroonetwo[2]] = 255;
+
+                img.SetPixel(row, 1, Color.FromArgb(rgb[0], rgb[1], rgb[2]));
+            }
+
+            using (Graphics grD = Graphics.FromImage(img))
+            {
+                Image copy = img.Clone(new Rectangle(0, 1, img.Width, 1), System.Drawing.Imaging.PixelFormat.DontCare);
+
+                for (int col = 2; col < img.Height - 1; col++)
+                {
+                    grD.DrawImage(copy, new Point(0, col));
+                }
+            }
+
+            pictureBox1.Image = img;
+            pictureBox1.Refresh();
+
+                bool finished = false;
+
+                while (!finished)
+                {
+                    Stopwatch stopWatchSub = new Stopwatch();
+                    stopWatchSub.Start();
+
+                    finished = true;
+
+                    for (int row = 0; row < img.Width - 1; row++)
+                    {
+                        if (img.GetPixel(row, 1).GetHue() < img.GetPixel(row + 1, 1).GetHue())
+                        {
+                            finished = false;
+                            break;
+                        }
+                    }
+
+                    for (int row = 0; row < img.Width; row++)
+                    {
+                        if (row + 1 != img.Width)
+                        {
+                            if (img.GetPixel(row, 1).GetHue() < img.GetPixel(row + 1, 1).GetHue())
+                            {
+                                Color second = img.GetPixel(row + 1, 1);
+
+                                img.SetPixel(row + 1, 1, img.GetPixel(row, 1));
+                                img.SetPixel(row, 1, second);
+                            }
+                        }
+                }
+
+                using (Graphics grD = Graphics.FromImage(img))
+                {
+                    Image copy = img.Clone(new Rectangle(0, 1, img.Width, 1), System.Drawing.Imaging.PixelFormat.DontCare);
+
+                    for (int col = 2; col < img.Height - 1; col++)
+                    {
+                        grD.DrawImage(copy, new Point(0, col));
+                    }
+                }
+
+                pictureBox1.Image = img;
+                pictureBox1.Refresh();
+
+                stopWatchSub.Stop();
+
+                string perSec = (0.001 / ((double)stopWatchSub.ElapsedMilliseconds / (double)1000)).ToString();
+
+                label2.Text = perSec;
+                this.label2.Refresh();
+                }
+            
+            
+            
+            pictureBox1.Image = img;
+            pictureBox1.Refresh();
+
+            stopwatch.Stop();
+
+            label4.Text = (stopwatch.ElapsedMilliseconds / 1000).ToString() + " seconds";
+            this.label4.Refresh();
+        }
+
+        private void btnQuick_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int width = 1024;
+            int height = 128;
+
+            Bitmap img = new Bitmap(width, height);
+
+            pictureBox2.Width = width;
+            pictureBox2.Height = height;
+
+            Random rnd = new Random();
+
+            int[] rgb;
+
+            for (int row = 0; row < img.Width; row++)
+            {
+                rgb = new int[3];
+
+                int[] zeroonetwo = { 0, 1, 2 };
+
+                int onetosix = rnd.Next(1, 7);
+
+                switch (onetosix)
+                {
+                    case 1:
+                        zeroonetwo = new int[] { 0, 1, 2 };
+                        break;
+                    case 2:
+                        zeroonetwo = new int[] { 1, 0, 2 };
+                        break;
+                    case 3:
+                        zeroonetwo = new int[] { 2, 0, 1 };
+                        break;
+                    case 4:
+                        zeroonetwo = new int[] { 2, 1, 0 };
+                        break;
+                    case 5:
+                        zeroonetwo = new int[] { 0, 2, 1 };
+                        break;
+                    case 6:
+                        zeroonetwo = new int[] { 1, 2, 0 };
+                        break;
+                }
+
+                rgb[zeroonetwo[0]] = rnd.Next(0, 256);
+                rgb[zeroonetwo[1]] = 0;
+                rgb[zeroonetwo[2]] = 255;
+
+                img.SetPixel(row, 1, Color.FromArgb(rgb[0], rgb[1], rgb[2]));
+            }
+
+            using (Graphics grD = Graphics.FromImage(img))
+            {
+                Image copy = img.Clone(new Rectangle(0, 1, img.Width, 1), System.Drawing.Imaging.PixelFormat.DontCare);
+
+                for (int col = 2; col < img.Height - 1; col++)
+                {
+                    grD.DrawImage(copy, new Point(0, col));
+                }
+            }
+
+            pictureBox2.Image = img;
+            pictureBox2.Refresh();
+
+
+
+            void Quicksort(IComparable[] elements, int left, int right)
+            {
+                int i = left, j = right;
+                IComparable pivot = elements[(left + right) / 2];
+
+                while (i <= j)
+                {
+                    while (elements[i].CompareTo(pivot) < 0)
+                    {
+                        i++;
+                    }
+
+                    while (elements[j].CompareTo(pivot) > 0)
+                    {
+                        j--;
+                    }
+
+                    if (i <= j)
+                    {
+                        // Swap
+                        IComparable tmp = elements[i];
+                        elements[i] = elements[j];
+                        elements[j] = tmp;
+
+                        i++;
+                        j--;
+                    }
+                }
+
+                // Recursive calls
+                if (left < j)
+                {
+                    Quicksort(elements, left, j);
+                }
+
+                if (i < right)
+                {
+                    Quicksort(elements, i, right);
+                }
+            }
+
+            //bool finished = false;
+
+            //List<Color> pivot = new List<Color>();
+
+            //List<int> currentRow = new List<int>();
+
+            //pivot.Add(img.GetPixel((img.Width + 1) / 2, 1));
+
+            //int i = 0;
+
+            //while (!finished)
+            //{
+            //    Stopwatch stopWatchSub = new Stopwatch();
+            //    stopWatchSub.Start();
+
+            //    List<List<Color>> colorListGreater = new List<List<Color>>();
+            //    List<List<Color>> colorListSmaller = new List<List<Color>>();
+
+            //    int currentp = 0;
+
+            //    for (int test = 0; test < pivot.Count; test++)
+            //    {
+            //        colorListGreater.Add(new List<Color>());
+            //        colorListSmaller.Add(new List<Color>());
+
+            //        for (int row = 0; row < (((img.Width + 1) / 2) - 1); row++)
+            //        {
+            //            Color current = img.GetPixel(row, 1);
+
+            //            if (current.GetHue() < pivot[test].GetHue())
+            //            {
+            //                colorListSmaller[currentp].Add(current);
+            //            }
+
+            //            if (current.GetHue() > pivot[test].GetHue())
+            //            {
+            //                colorListGreater[currentp].Add(current);
+            //            }
+            //        }
+
+            //        for (int row = (((img.Width + 1) / 2) + 1); row < img.Width; row++)
+            //        {
+            //            Color current = img.GetPixel(row, 1);
+
+            //            if (current.GetHue() < pivot[test].GetHue())
+            //            {
+            //                colorListSmaller[currentp].Add(current);
+            //            }
+
+            //            if (current.GetHue() > pivot[test].GetHue())
+            //            {
+            //                colorListGreater[currentp].Add(current);
+            //            }
+            //        }
+
+
+            //        foreach (Color c in colorListSmaller[currentp])
+            //        {
+            //            currentRow.Add(0);
+
+            //            img.SetPixel(currentRow[currentp], 1, c);
+
+            //            currentRow[currentp]++;
+
+            //            using (Graphics grD = Graphics.FromImage(img))
+            //            {
+            //                Image copy = img.Clone(new Rectangle(0, 1, img.Width, 1), System.Drawing.Imaging.PixelFormat.DontCare);
+
+            //                for (int col = 2; col < img.Height - 1; col++)
+            //                {
+            //                    grD.DrawImage(copy, new Point(0, col));
+            //                }
+            //            }
+
+            //            pivot.Add(img.GetPixel(currentRow[currentp], 1));
+
+            //            pictureBox2.Image = img;
+            //            pictureBox2.Refresh();
+            //        }
+
+            //        img.SetPixel(currentRow[currentp], 1, pivot[test]);
+
+            //        foreach (Color c in colorListGreater[currentp])
+            //        {
+            //            currentRow[currentp] = 0;
+
+            //            img.SetPixel(currentRow[currentp], 1, c);
+
+            //            currentRow[currentp]++;
+
+            //            using (Graphics grD = Graphics.FromImage(img))
+            //            {
+            //                Image copy = img.Clone(new Rectangle(0, 1, img.Width, 1), System.Drawing.Imaging.PixelFormat.DontCare);
+
+            //                for (int col = 2; col < img.Height - 1; col++)
+            //                {
+            //                    grD.DrawImage(copy, new Point(0, col));
+            //                }
+            //            }
+
+            //            pivot.Add(img.GetPixel(currentRow[currentp], 1));
+
+            //            pictureBox2.Image = img;
+            //            pictureBox2.Refresh();
+            //        }
+
+            //        currentp++;
+            //    }
+
+            //    foreach (Color p in pivot)
+            //    {
+
+            //    }
+
+
+
+            //    pictureBox2.Image = img;
+            //    pictureBox2.Refresh();
+
+            //    stopWatchSub.Stop();
+
+            //    string perSec = (0.001 / ((double)stopWatchSub.ElapsedMilliseconds / (double)1000)).ToString();
+
+            //    label2.Text = perSec;
+            //    this.label2.Refresh();
+
+            //    i++;
+            //}
+
+
+            //for (int row = 0; row < img.Width - 1; row++)
+            //{
+            //    if (img.GetPixel(row, 1).GetHue() < img.GetPixel(row + 1, 1).GetHue())
+            //    {
+            //        finished = false;
+            //        break;
+            //    }
+            //}
+
+            //for (int row = 0; row < img.Width; row++)
+            //{
+            //    if (row + 1 != img.Width)
+            //    {
+            //        if (img.GetPixel(row, 1).GetHue() < img.GetPixel(row + 1, 1).GetHue())
+            //        {
+            //            Color second = img.GetPixel(row + 1, 1);
+
+            //            img.SetPixel(row + 1, 1, img.GetPixel(row, 1));
+            //            img.SetPixel(row, 1, second);
+            //        }
+            //    }
+            //}
+
+
+            pictureBox2.Image = img;
+            pictureBox2.Refresh();
+
+            stopwatch.Stop();
+
+            label4.Text = (stopwatch.ElapsedMilliseconds / 1000).ToString() + " seconds";
+            this.label4.Refresh();
+        }
+
+        public static int[] GenerateRGB()
+        {
+            Random rnd = new Random();
+
+            int[] rgb = new int[3];
+
+            int[] zeroonetwo = { 0, 1, 2 };
+
+            int onetosix = rnd.Next(1, 7);
+
+            switch (onetosix)
+            {
+                case 1:
+                    zeroonetwo = new int[] { 0, 1, 2 };
+                    break;
+                case 2:
+                    zeroonetwo = new int[] { 1, 0, 2 };
+                    break;
+                case 3:
+                    zeroonetwo = new int[] { 2, 0, 1 };
+                    break;
+                case 4:
+                    zeroonetwo = new int[] { 2, 1, 0 };
+                    break;
+                case 5:
+                    zeroonetwo = new int[] { 0, 2, 1 };
+                    break;
+                case 6:
+                    zeroonetwo = new int[] { 1, 2, 0 };
+                    break;
+            }
+
+            rgb[zeroonetwo[0]] = rnd.Next(0, 256);
+            rgb[zeroonetwo[1]] = 0;
+            rgb[zeroonetwo[2]] = 255;
+
+            return rgb;
+        }
+    }
+}
